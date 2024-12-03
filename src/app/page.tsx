dcfffada6c4cd834571299d47ea0369c449da2ed
Hardik Rathod <hardikrathod1219@gmail.com>
2024-12-03 10:26:27 +0530

    feat: Adding layer store provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,29 @@
 
 import { Editor } from "@/components/editor";
 import { ImageStore } from "@/lib/image-store";
+import { LayerStore } from "@/lib/layer-store";
 
 export default function Home() {
     return (
         <main>
-            <ImageStore.Provider initialValue={{ generating: false }}>
-                <Editor />
-            </ImageStore.Provider>
+            <LayerStore.Provider
+                initialValue={{
+                    layerComparisonMode: false,
+                    layers: [
+                        {
+                            id: crypto.randomUUID(),
+                            url: "",
+                            height: 0,
+                            width: 0,
+                            publicId: "",
+                        },
+                    ],
+                }}
+            >
+                <ImageStore.Provider initialValue={{ generating: false }}>
+                    <Editor />
+                </ImageStore.Provider>
+            </LayerStore.Provider>
         </main>
     );
 }
